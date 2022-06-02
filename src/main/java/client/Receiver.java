a67c0684a52b2e23cb5ff0cc625d56972f3b5d7f
오지훈 <huni1006@lezhin.com>
2022-06-02 16:43:40 +0900

    smf decoder

--- src/main/java/client/Receiver.java
+++ src/main/java/client/Receiver.java
@@ -2,47 +2,45 @@ package client;
 
 import client.format.SimpleMessageFormat;
 import common.view.console.ConsoleMessage;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.util.Objects;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import static util.IoUtil.createObjectInputStream;
+import static util.IoUtil.createReader;
 
 @Slf4j
 class Receiver {
-    private static final SimpleMessageFormat END_CONNECTION = null;
+    private static final String END_CONNECTION = null;
     public static final char[] BUFFER = new char[8192];
 
-    private final ObjectInputStream in;
+    private final BufferedReader in;
 
-    private Receiver(@NonNull ObjectInputStream in) {
+    private Receiver(@NonNull BufferedReader in) {
         this.in = in;
     }
 
     public static Receiver create(@NonNull InputStream inputStream){
-        return new Receiver(createObjectInputStream(inputStream));
+        return new Receiver(createReader(inputStream));
     }
 
     public void waitAndThenGetMsg() {
-        SimpleMessageFormat simpleMessageFormat = END_CONNECTION;
+        String smfMessage = END_CONNECTION;
         try{
-            while( (simpleMessageFormat = (SimpleMessageFormat) in.readObject()) != END_CONNECTION ){
-                ConsoleMessage consoleMessage = simpleMessageFormat.createConsoleForm();
+            while(!Objects.equals(smfMessage = in.readLine(), END_CONNECTION)) {
 
-                log.info("From server : {}", consoleMessage.create());
+                log.info("From server : {}", smfMessage);
 
-//                SimpleMessageFormat simpleMessageFormat = SimpleMessageFormat.create(msg);
-//
-//                log.info(simpleMessageFormat.decodeForConsole());
-//
-//                FileRecorder fileRecorder = new FileRecorder("서버");
-//                fileRecorder.accept(simpleMessageFormat.decodeForFile());
+
+
+
+
             }
         } catch(IOException e) {
             throw new RuntimeException("Fail to receive msg.",e);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         }
     }
 }
