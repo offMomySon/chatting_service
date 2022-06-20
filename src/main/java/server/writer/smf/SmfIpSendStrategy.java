6082634aa8ace971dbf43547d264b6d900c1a843
오지훈 <huni1006@lezhin.com>
2022-06-20 20:27:25 +0900

    Refac SendStrategies

--- src/main/java/server/writer/smf/SmfIpSendStrategy.java
+++ src/main/java/server/writer/smf/SmfIpSendStrategy.java
@@ -10,22 +10,26 @@ import server.v5.MessageWriter;
 import static server.v5.Usage.SOCKET;
 
 public class SmfIpSendStrategy implements SmfSendStrategy{
-    private final List<Destination> addresses;
+    private final List<Destination> destinations;
+    private final SimpleMessageFormat message;
     private final MessageWriter messageWriter;
 
-    public SmfIpSendStrategy(@NonNull List<Destination> addresses, @NonNull MessageWriter messageWriter) {
-        this.addresses = addresses;
+    public SmfIpSendStrategy(@NonNull List<Destination> destinations, @NonNull SimpleMessageFormat message, @NonNull MessageWriter messageWriter) {
+        this.destinations = destinations;
+        this.message = message;
         this.messageWriter = messageWriter;
     }
 
-    public static SmfIpSendStrategy from(@NonNull List<Address> addresses,  @NonNull MessageWriter messageWriter){
-        List<Destination> destinations = addresses.stream().map(address -> new Destination(address, SOCKET)).collect(Collectors.toUnmodifiableList());
+    public static SmfIpSendStrategy from(@NonNull List<Address> addresses, @NonNull SimpleMessageFormat message, @NonNull MessageWriter messageWriter){
+        List<Destination> destinations = addresses.stream()
+            .map(address -> new Destination(address, SOCKET))
+            .collect(Collectors.toUnmodifiableList());
 
-        return new SmfIpSendStrategy(destinations, messageWriter);
+        return new SmfIpSendStrategy(destinations, message, messageWriter);
     }
 
     @Override
-    public void send(SimpleMessageFormat message) {
-        addresses.forEach(addressDirection -> messageWriter.write(addressDirection, message));
+    public void send() {
+        destinations.forEach(addressDirection -> messageWriter.write(addressDirection, message));
     }
 }
--- src/main/java/server/writer/smf/SmfSendStrategy.java
+++ src/main/java/server/writer/smf/SmfSendStrategy.java
@@ -1,8 +1,5 @@
 package server.writer.smf;
 
-import lombok.NonNull;
-import server.message.smf.SimpleMessageFormat;
-
 public interface SmfSendStrategy {
-    void send(@NonNull SimpleMessageFormat simpleMessageFormat);
+    void send();
 }
