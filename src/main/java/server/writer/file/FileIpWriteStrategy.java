2166549d9789cddca53e81f5e8cff529c14d0aeb
오지훈 <huni1006@lezhin.com>
2022-06-20 22:12:52 +0900

    Impl NewCmdParser

--- src/main/java/server/writer/file/FileIpWriteStrategy.java
+++ src/main/java/server/writer/file/FileIpWriteStrategy.java
@@ -7,21 +7,20 @@ import server.Address;
 import server.message.file.FileMessage;
 import server.v5.Destination;
 import server.v5.MessageWriter;
-import server.writer.smf.SmfIpSendStrategy;
 import static server.v5.Usage.FILE;
 
 public class FileIpWriteStrategy implements FileWriteStrategy{
     private final List<Destination> addresses;
     private final MessageWriter messageWriter;
 
-    public FileIpWriteStrategy(@NonNull List<Destination> addresses, MessageWriter messageWriter) {
+    private FileIpWriteStrategy(@NonNull List<Destination> addresses, MessageWriter messageWriter) {
         this.addresses = addresses;
         this.messageWriter = messageWriter;
     }
-    public static SmfIpSendStrategy from(@NonNull List<Address> addresses, @NonNull MessageWriter messageWriter){
+    public static FileIpWriteStrategy from(@NonNull List<Address> addresses, @NonNull MessageWriter messageWriter){
         List<Destination> destinations = addresses.stream().map(address -> new Destination(address, FILE)).collect(Collectors.toUnmodifiableList());
 
-        return new SmfIpSendStrategy(destinations, messageWriter);
+        return new FileIpWriteStrategy(destinations, messageWriter);
     }
 
     @Override
