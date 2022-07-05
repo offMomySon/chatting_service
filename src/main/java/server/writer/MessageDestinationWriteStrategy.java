d3283d976eb253d3219b42c1df58692f1a2f325c
오지훈 <huni1006@lezhin.com>
2022-07-06 01:34:31 +0900

    refac write strategy

---
+++ src/main/java/server/writer/MessageDestinationWriteStrategy.java
@@ -0,0 +1,35 @@
+package server.writer;
+
+import java.util.List;
+import lombok.NonNull;
+import server.v5.Destination;
+import server.v5.Message;
+import server.v5.MessageWriter;
+
+/**
+ * 역할.
+ * 지정한 destination 에 message 를 쓰는 역할.
+ *
+ * 아래의 구조의 메서드는 목적지가 destination 이 아니라,
+ * 지정한 address 에 대해 usage 에 맞는 message 를 쓴다의 범용적 기능의 역할.
+ * public void write(@NonNull Message message, @NonNull Usage usage)
+ *
+ * 즉, instance 에 따라 적절한 행위를 수행하는 시스템 도메인이 아니라,
+ * 지정한 address 에 대해 message, usage 를 이용해 모든 타입의 메세지를 출력하는
+ * 범용적 기능을 가진 객체 였다.
+ * -> 도메인 관심사가 흩어진다. ( destination 에 message 를 쓰는 것이 아니라. address 와 usage 의 조합한 destination 에 message 를 쓴다. 가 됨.)
+ */
+public class MessageDestinationWriteStrategy implements MessageWriteStrategy{
+    private final MessageWriter messageWriter;
+    private final List<Destination> destinations;
+
+    public MessageDestinationWriteStrategy(@NonNull MessageWriter messageWriter, @NonNull List<Destination> destinations) {
+        this.messageWriter = messageWriter;
+        this.destinations = destinations;
+    }
+
+    @Override
+    public void write(@NonNull Message message) {
+        destinations.forEach(destination -> messageWriter.write(destination, message));
+    }
+}
---
+++ src/test/java/server/writer/MessageDestinationWriteStrategyTest.java
@@ -0,0 +1,107 @@
+package server.writer;
+
+import common.MessageOwner;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import server.Address;
+import server.message.file.FileMessage;
+import server.v5.Destination;
+import server.v5.Message;
+import server.v5.MessageWriter;
+import server.v5.Usage;
+import static server.v5.Usage.FILE;
+
+/**
+ * 1)
+ * destination 에 message 를 쓰기 위함인데,
+ * 아래의 코드에서 addresses 를 기반으로 객체를 생성하는것에 이상함을 느꼈다.
+ * MessageIpWriteStrategy messageWriteStrategy = new MessageIpWriteStrategy(messageWriter, addresses);
+ *
+ * 또한 MessageWriter 생성에도 실제로는 destination 이 필요한데,
+ * address, usage 를 넘겨줘야한다는 부분에서 이상함을 느꼈다.
+ * MessageWriter messageWriter = MessageWriter.of(sourceMap, usage);
+ *
+ * 2)
+ * destination 에 메세지를 쓰는 것이기 때문에, address 대신 destination 으로 생성하는게 맞다고 생각함.
+ * 객체생성을 destination 으로 변경하였다.
+ *
+ * 3)
+ * [1] 테스트가 쉬워짐.
+ * 테스트 param 에서 usage 가 필요없어짐.
+ *
+ * [2] MessageWriter 객체 생성이 한번에 이루어짐.
+ * 생성자에서 address, usage 를 받아 내부에서 변환하는 작업이 필요없어짐.
+ *
+ * [3] MessageIpWriteStrategy 에서 destination 을 관리함에 따라
+ * write 메서드에서 어떤 usage 를 받아야할지에 대한 고민이 사라짐. ( 적절한 message 를 던져주면 된다.)
+ *
+ *
+ * 키워드
+ * 범용적인 기능의 객체.
+ * 시스템 도메인 객체.
+ *
+ */
+class MessageDestinationWriteStrategyTest {
+    @DisplayName("지정한 address 에 대해 message 를 출력합니다.")
+    @ParameterizedTest
+    @MethodSource("provideSourceAndDestination")
+    void test2(Map<Destination, OutputStream> sourceMap, List<Destination> destinations){
+        //given
+        Message message = new FileMessage(LocalDateTime.now(), MessageOwner.INFO, "this is message.");
+
+        MessageWriter messageWriter = MessageWriter.of(sourceMap);
+        MessageDestinationWriteStrategy messageWriteStrategy = new MessageDestinationWriteStrategy(messageWriter, destinations);
+
+        //when
+        messageWriteStrategy.write(message);
+
+        List<String> actualMessage = new LinkedList<>();
+        for(Destination destination : destinations){
+            actualMessage.add(sourceMap.get(destination).toString());
+        }
+
+        //then
+        Assertions.assertThat(actualMessage).allSatisfy(actual->{
+            Assertions.assertThat(actual).isEqualTo(message.create());
+        });
+    }
+
+    private static Stream<Arguments> provideSourceAndDestination(){
+        Usage usage = FILE;
+
+        List<Destination> sourceDestination = List.of(
+            new Destination(new Address("127.0.0.0"), usage),
+            new Destination(new Address("127.0.0.1"), usage),
+            new Destination(new Address("127.0.0.2"), usage),
+            new Destination(new Address("127.0.0.3"), usage)
+        );
+        List<OutputStream> sourceOutputStreams = List.of(
+            new ByteArrayOutputStream(),
+            new ByteArrayOutputStream(),
+            new ByteArrayOutputStream(),
+            new ByteArrayOutputStream()
+        );
+
+        Map<Destination, OutputStream> sourceMap = new HashMap<>();
+        IntStream.range(0, sourceDestination.size()).forEach(i-> sourceMap.put(sourceDestination.get(i), sourceOutputStreams.get(i)));
+
+        List<Destination> destinations = List.of(
+            new Destination(new Address("127.0.0.0"), usage),
+            new Destination(new Address("127.0.0.1"), usage)
+        );
+
+        return Stream.of(Arguments.of(sourceMap, destinations));
+    }
+}
